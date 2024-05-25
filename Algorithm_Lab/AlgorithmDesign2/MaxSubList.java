e50f336aef6bd10f123e04f136d456e62f73c30a
이승형 <102710821+RHPLUSSEUNG@users.noreply.github.com>
2024-05-25 15:54:20 +0900

    feat: 동적프로그래밍 2.2
    합이 최대인 부분 리스트 구하기
    출력 추가 구현

---
+++ Algorithm_Lab/AlgorithmDesign2/MaxSubList.java
@@ -0,0 +1,50 @@
+public class MaxSubList
+{
+    public static double maxSubList(double[] A, int n)
+    {
+        double[] B;
+        double max;
+
+        B = new double[n];
+        max = B[0] = A[0];
+        int start = 0;
+        int indexLength = 0;
+
+        for(int i = 1; i < n; i++)
+        {
+            //B[i] = (B[i-1] <= 0) ? A[i] : B[i-1] + A[i];
+
+            if(B[i-1] <= 0)
+            {
+                B[i] = A[i];
+                indexLength = 0;
+            }
+            else
+            {
+                B[i] = B[i-1] + A[i];
+            }
+            
+            indexLength++;
+
+            if(max < B[i])
+            {
+                max = B[i];
+                start = i;                
+            }
+        }
+
+        for(int i = start - indexLength + 2; i <= start; i++)
+        {
+            System.out.println(A[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args)
+    {
+        double[] ex = {2.3, 3.2, -4.5, 2.1, -5.3, 3.6, 4.1, -2.3, 3.5, -4.5};
+        int n = ex.length;
+
+        maxSubList(ex, n);
+    }
+}
